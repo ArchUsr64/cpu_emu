ee8afe874832168b16f97155ca8a6b533291e125
ArchUsr64 <ArchUsr64>
2022-08-18 09:53:52 +0530

    Implemented logger

--- src/term.rs
+++ src/term.rs
@@ -1,38 +1,23 @@
 extern crate ctrlc;
 extern crate termsize;
 
-pub struct TerminalSize {
-	pub cols: u32,
-	pub rows: u32,
-}
-
-impl std::fmt::Display for TerminalSize {
-	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-		write!(f, "({}, {})", self.rows, self.cols)
-	}
-}
-
-pub fn get_terminal_size() -> TerminalSize {
-	let mut return_struct: TerminalSize = TerminalSize { rows: 0, cols: 0 };
-	termsize::get().map(|size| {
-		return_struct.rows = size.rows as u32;
-		return_struct.cols = size.cols as u32;
-	});
-	return_struct
-}
-
-pub fn reset_terminal() {
-	print!("\x1B[2J\x1B[1;1H");
-}
-
-pub fn draw_grayscale(val: f32) {
-	print_ansi_string(val, " ".to_string());
-}
-
-pub fn print_ansi_string(val: f32, text: String) {
-	let mut temp_val = val;
-	temp_val = 232.0 + temp_val * 23.0;
-	print!("\x1b[48;5;{}m{}", temp_val as u16, text);
+#[macro_export]
+macro_rules! log {
+	($y:expr, $($x:expr), * ) => {
+		if $y {
+			let mut time = std::time::SystemTime::now()
+				.duration_since(std::time::UNIX_EPOCH)
+				.unwrap()
+				.as_secs();
+			let time_zone_offset = (5 * 3600) + (30 * 60);
+			time += time_zone_offset;
+			let hours = (time / 3600) % 24;
+			let minutes = (time / 60) % 60;
+			let seconds = time % 60;
+			print!("[{:02}:{:02}:{:02}] ", hours, minutes, seconds);
+			println!("[DEBUG] {}" ,format!($($x,)*));
+		};
+	};
 }
 
 pub fn ctrl_c_init() {
